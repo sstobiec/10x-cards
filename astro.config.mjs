2b8f382b7ef1a056c5ea6fa4577b691fabe9c757
Sebastian Stobiecki <sebastianstobiecki@Sebastians-MacBook-Pro.local>
2025-12-06 08:35:28 +0100

    d

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,8 +14,11 @@ export default defineConfig({
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      resolve: {
+        conditions: ["workerd", "worker", "browser"],
+      },
+    },
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
